c65eee6c6e5a9ce96fcb78db044a6b18e15a7001
jpc496 <jpc496@msstate.edu>
2023-08-17 14:38:30 -0500

    Add working buttons for "about" page, add new js script

---
+++ assets/scripts/image-with-button.js
@@ -0,0 +1,55 @@
+class ImageWithText extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    static get observedAttributes() {
+      return ['image-src', 'click-link','link-text'];
+    }
+  
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (name === 'image-src') {
+        this.imageSrc = newValue;
+        this.render();
+      }
+      if (name === 'link-text') {
+        this.linkText = newValue;
+        this.render();
+      }
+      if (name === 'click-link') {
+        this.clickLink = newValue;
+        this.render();
+      }
+    }
+  
+    connectedCallback() {
+      this.imageSrc = this.getAttribute('image-src') || '#'; // Default image source
+      this.clickLink = this.getAttribute('click-link') || '#'; // Default link
+      this.linkText = this.getAttribute('link-text') || 'NAME_MISSING'; // Default link text
+      
+        
+      this.render();
+    }
+  
+    render() {
+      this.innerHTML = `
+        <style>
+          .image-with-text {
+            display: inline-block;
+            text-align: center;
+            margin: 10px;
+          }
+        </style>
+        <div class="image-with-text">
+          <img 
+            src="${this.imageSrc}"
+            alt="${this.linkText}">
+          <p></p>
+          <a href="${this.clickLink}" class="button">${this.linkText}</a>
+        </div>
+      `;
+    }
+  }
+  
+  customElements.define('image-with-button', ImageWithText);
+  
